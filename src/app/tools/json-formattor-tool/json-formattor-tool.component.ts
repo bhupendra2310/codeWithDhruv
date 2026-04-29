a539b31707b2df098105e407eba5e13a3aa0bd3a
Bhupendra Kushwah <bhupenkushwah23@gmail.com>
2026-04-29 20:52:29 +0530

    add google verification file

--- src/app/tools/json-formattor-tool/json-formattor-tool.component.ts
+++ src/app/tools/json-formattor-tool/json-formattor-tool.component.ts
@@ -42,4 +42,40 @@ constructor( private toast:ToastrService) {
       console.error('Failed to copy:', err);
     });
   }
+
+
+  data = {
+    "students": [
+      { "firstName": "ram", "lastName": "Das" },
+      { "firstName": "John", "lastName": "Smith" },
+      { "firstName": "Peter", "lastName": "nick" }
+    ]
+  };
+
+
+  activeIndex: number | null = null;
+
+  faqs = [
+    {
+      question: 'What is JSON ?',
+      answer: 'JavaScript Object Notation (JSON) is an open data interchange format that is both human and machine-readable. JSON is often used when data is sent from a server to a web page'
+    },
+    {
+      question: 'Why should I use a JSON formatter?',
+      answer: 'A JSON formatter helps organize messy data into a clean, readable format, making it easier to understand and debug.'
+    },
+    {
+      question: 'Is this JSON formatter tool free to use?',
+      answer: 'Yes, the tool is completely free and lets you format, view, and validate JSON data instantly without any signup.'
+    },
+    {
+      question: 'Can a JSON formatter detect errors?',
+      answer: 'It can quickly spot syntax errors in your JSON, so you can fix issues without wasting time.'
+    }
+  ];
+
+  toggle(index: number) {
+    this.activeIndex = this.activeIndex === index ? null : index;
+  }
+
 }
